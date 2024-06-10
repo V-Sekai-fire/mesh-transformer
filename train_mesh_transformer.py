2fee6c8a2686dda3cace9d784e6fa8cb094b6955
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-06-10 10:54:52 -0700

    Update the license.

--- train_mesh_transformer.py
+++ train_mesh_transformer.py
@@ -1,4 +1,4 @@
-# SPDX-License-Identifier: MIT
+# SPDX-License-Identifier: MIT OR Apache-2.0
 # Copyright (c) 2024 MarcusLoppe & K. S. Ernest (iFire) Lee
 
 from dagster import op, execute_job, job, reconstructable, DagsterInstance
