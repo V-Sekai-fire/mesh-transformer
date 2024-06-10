6b10536e45dcdd909d8140ecde9e627fb2da7391
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-06-10 09:51:34 -0700

    Save training scripts..

---
+++ train_autoencoder.py
@@ -0,0 +1,131 @@
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2024 MarcusLoppe & K. S. Ernest (iFire) Lee
+
+from dagster import op, job
+import torch
+import datetime
+import random
+import tqdm
+import os
+from meshgpt_pytorch import MeshAutoencoderTrainer, MeshAutoencoder, MeshDataset, mesh_render
+from dagster import execute_job, reconstructable, DagsterInstance, AssetMaterialization, In, Out
+
+AUTOENCODER_EPOCHS = 1 # 480
+
+@op
+def create_autoencoder(context):
+    autoencoder = MeshAutoencoder( 
+        decoder_dims_through_depth =  (128,) * 6 + (192,) * 12 + (256,) * 24 + (384,) * 6,    
+        dim_codebook = 192,  
+        dim_area_embed = 16,
+        dim_coor_embed = 16, 
+        dim_normal_embed = 16,
+        dim_angle_embed = 8,
+        attn_decoder_depth  = 4,
+        attn_encoder_depth = 2
+    ).to("cuda")     
+    return autoencoder
+
+@op
+def load_datasets(context):
+    dataset = MeshDataset.load("./shapenet_250f_2.2M_84_labels_2156_10_min_x1_aug.npz")  
+    # dataset2 = MeshDataset.load("./objverse_250f_45.9M_3086_labels_53730_10_min_x1_aug.npz")
+    # dataset.data.extend(dataset2.data)  
+    # dataset2 = MeshDataset.load("./shapenet_250f_21.9M_84_labels_21560_10_min_x10_aug.npz")  
+    # dataset.data.extend(dataset2.data)
+    # dataset2 = MeshDataset.load("./objverse_250f_229.7M_3086_labels_268650_10_min_x5_aug.npz")
+    # dataset.data.extend(dataset2.data) 
+    # dataset.sort_dataset_keys()
+    return dataset
+
+
+@op
+def train_autoencoder(context, autoencoder, dataset):
+    batch_size=16
+    grad_accum_every =4
+    learning_rate = 1e-3
+    autoencoder.commit_loss_weight = 0.2
+    autoencoder_trainer = MeshAutoencoderTrainer(model =autoencoder ,warmup_steps = 10, dataset = dataset, num_train_steps=100,
+                                             batch_size=batch_size,
+                                             grad_accum_every = grad_accum_every,
+                                             learning_rate = learning_rate,
+                                             checkpoint_every_epoch=5)
+    loss = autoencoder_trainer.train(AUTOENCODER_EPOCHS, diplay_graph= False)   
+    return autoencoder
+
+@op(
+    ins={"autoencoder": In(metadata={
+            "time": datetime.datetime.now(datetime.timezone.utc).isoformat().replace(":", "_"),
+            "model_size_bytes": str(os.path.getsize("./MeshGPT-autoencoder.pt")),
+        })},
+    out={
+        "trained_autoencoder": Out(is_required=False)
+    },
+)
+def save_model(context, autoencoder):
+    pkg = dict( model = autoencoder.state_dict(), )
+    filename = "./MeshGPT-autoencoder.pt"
+    torch.save(pkg, filename)
+    return autoencoder
+
+@op(
+    ins={"dataset": In(),
+         "autoencoder": In(metadata={
+            "time": datetime.datetime.now(datetime.timezone.utc).isoformat().replace(":", "_"),
+            "model_size_bytes": str(os.path.getsize("./MeshGPT-autoencoder.pt")),
+        })},
+    out={
+        "autoencoder": Out(),
+        "mse_obj": Out()
+    },
+)
+def evaluate_model(context, autoencoder, dataset):
+    min_mse, max_mse = float('inf'), float('-inf')
+    min_coords, min_orgs, max_coords, max_orgs = None, None, None, None
+    random_samples, random_samples_pred, all_random_samples = [], [], []
+    total_mse, sample_size = 0.0, 200
+
+    random.shuffle(dataset.data)
+    autoencoder = autoencoder.to("cuda")
+    for item in tqdm.tqdm(dataset.data[:sample_size]):  # Use tqdm.tqdm here
+        item['faces'] = item['faces'].to("cuda")
+        item['vertices'] = item['vertices'].to("cuda")
+        item['face_edges'] = item['face_edges'].to("cuda")
+        codes = autoencoder.tokenize(vertices=item['vertices'], faces=item['faces'], face_edges=item['face_edges']) 
+
+        codes = codes.flatten().unsqueeze(0)
+        codes = codes[:, :codes.shape[-1] // autoencoder.num_quantizers * autoencoder.num_quantizers] 
+
+        coords, mask = autoencoder.decode_from_codes_to_faces(codes)
+        orgs = item['vertices'][item['faces']].unsqueeze(0)
+
+        mse = torch.mean((orgs.view(-1, 3).cpu() - coords.view(-1, 3).cpu())**2)
+        total_mse += mse 
+
+        if mse < min_mse: min_mse, min_coords, min_orgs = mse, coords, orgs
+        if mse > max_mse: max_mse, max_coords, max_orgs = mse, coords, orgs
+
+        if len(random_samples) <= 30:
+            random_samples.append((coords, mask)) 
+        else:
+            all_random_samples.extend([ random_samples])
+            random_samples, random_samples_pred = [], []
+
+    print(f'MSE AVG: {total_mse / sample_size:.10f}, Min: {min_mse:.10f}, Max: {max_mse:.10f}')    
+    mesh_render.save_rendering('./mse_rows.obj', all_random_samples)
+    with open("./mse_rows.obj", "r") as file:
+        mse_obj = file.read()
+        
+    return autoencoder, mse_obj
+
+@job
+def train_autoencoder_job():
+    autoencoder = create_autoencoder()
+    dataset = load_datasets()
+    trained_autoencoder = train_autoencoder(autoencoder, dataset)
+    evaluate_model(autoencoder, dataset)
+    save_model(trained_autoencoder)
+
+if __name__ == "__main__":
+    instance = DagsterInstance.get()
+    result = execute_job(reconstructable(train_autoencoder_job), instance=instance)
---
+++ train_mesh_transformer.py
@@ -0,0 +1,89 @@
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2024 MarcusLoppe & K. S. Ernest (iFire) Lee
+
+from dagster import op, execute_job, job, reconstructable, DagsterInstance
+import datetime
+import torch
+import random
+from tqdm import tqdm 
+from meshgpt_pytorch import mesh_render 
+
+from dagster import execute_job, reconstructable, DagsterInstance
+from train_autoencoder import train_autoencoder_job, create_autoencoder, train_autoencoder, save_model, evaluate_model
+
+from meshgpt_pytorch import (
+    MeshTransformerTrainer,
+    MeshAutoencoderTrainer,
+    MeshAutoencoder,
+    MeshTransformer,
+    MeshDataset
+)
+
+AUTOENCODER_EPOCHS = 1 # 480
+TRANSFORMER_TOTAL_EPOCHS = 1 # 740
+TRANSFORMER_EPOCHS_PER_SAVE = 25
+
+@op
+def load_datasets(context):
+    dataset = MeshDataset.load("./objverse_250f_45.9M_3086_labels_53730_10_min_x1_aug.npz")  
+    dataset2 = MeshDataset.load("./objverse_250f_229.7M_3086_labels_268650_10_min_x5_aug.npz")
+    dataset.data.extend(dataset2.data)  
+    dataset2 = MeshDataset.load("./shapenet_250f_2.2M_84_labels_2156_10_min_x1_aug.npz")  
+    dataset.data.extend(dataset2.data)  
+    dataset2 = MeshDataset.load("./shapenet_250f_21.9M_84_labels_21560_10_min_x10_aug.npz")  
+    dataset.data.extend(dataset2.data) 
+    dataset.sort_dataset_keys()
+    return dataset
+
+@op
+def create_transformer(context, autoencoder):
+    transformer = MeshTransformer(
+        autoencoder,
+        dim = 768,
+        coarse_pre_gateloop_depth =2,
+        fine_pre_gateloop_depth= 2,
+        attn_depth = 12,
+        attn_heads = 12,
+        fine_cross_attend_text = True,
+        text_cond_with_film = False,
+        cross_attn_num_mem_kv = 4,
+        num_sos_tokens = 1,
+        dropout  = 0.0,
+        max_seq_len = 1500,
+        fine_attn_depth = 2,
+        condition_on_text = True,
+        gateloop_use_heinsen = False,
+        text_condition_model_types = "bge",
+        text_condition_cond_drop_prob = 0.25,
+    ).to("cuda")
+
+    pkg = torch.load("./MeshGPT-transformer_trained_base.pt")
+    transformer.load_state_dict(pkg['model'],strict=False)
+    
+    return transformer
+
+@op
+def train_transformer(context, transformer, dataset):
+    batch_size =16
+    grad_accum_every =4
+    trainer = MeshTransformerTrainer(model = transformer,warmup_steps = 10,grad_accum_every=grad_accum_every,num_train_steps=100, dataset = dataset,
+                                     learning_rate = 1e-4, batch_size=batch_size, checkpoint_every_epoch = 25)
+
+    for i in range(TRANSFORMER_EPOCHS_PER_SAVE, TRANSFORMER_TOTAL_EPOCHS + 1, TRANSFORMER_EPOCHS_PER_SAVE):
+        trainer.train(i, stop_at_loss = 0.00005)
+        pkg = dict( model = transformer.state_dict(), )
+        torch.save(pkg, str(f"./MeshGPT-transformer_trained_{i}.pt"))
+
+@job
+def training_pipeline():
+    autoencoder = create_autoencoder()
+    dataset = load_datasets()
+    trained_autoencoder = train_autoencoder(autoencoder, dataset)
+    saved_model = save_model(trained_autoencoder)
+    transformer = create_transformer(trained_autoencoder)
+    train_transformer(transformer, dataset)
+    evaluate_model(trained_autoencoder, dataset)
+
+if __name__ == "__main__":
+    instance = DagsterInstance.get()
+    result = execute_job(reconstructable(training_pipeline), instance=instance)
